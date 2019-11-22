3fe4923eecca08e3bf42a3f57bddac49cae22940
JasonTheCoder-Java <58065758+JasonTheCoder-Java@users.noreply.github.com>
2019-11-21 23:16:15 -0800

    Update countInRange.java

--- chapter7/countInRange.java
+++ chapter7/countInRange.java
@@ -6,7 +6,7 @@
 public int countInRange(int[] arr, int min, int max) {
     int count = 0;
     for(int i = 0; i < arr.length; i++) {
-        if(min <= arr[i] && arr[i] <= max)
+        if(min >= arr[i] && arr[i] <= max)
             count++;
     }
     
